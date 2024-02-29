648d9f7bf2e980f9e1de5e0cc83dd584ec7fae00
masoodpk <masoodpk555@gmail.com>
2024-02-29 14:34:22 +0530

    .env file added,bcrypt jsonwebtoken

---
+++ server/models/login.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+    username:{
+        type:String
+    },
+email:{
+    type:String
+},
+password:{
+    type:String
+},
+confirmPassword:{
+    type:String
+},
+profile:{
+    type:String
+}
+
+
+});
+
+export default mongoose.model.Logins || mongoose.model("Login",schema);
---
+++ server/request-handler.js
@@ -0,0 +1,6 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+import loginModel from "./models/login.model.js";
+
+import path from "path";
+const { sign } = jwt;
